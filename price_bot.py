a3ec730d1ba45776b2df31757dcaca5bd9153061
Q8Shinobi <98072037+Q8Shinobi@users.noreply.github.com>
2023-02-15 21:17:03 -0800

    Update price_bot.py

--- price_bot.py
+++ price_bot.py
@@ -1,60 +1,71 @@
-#from requests import HTMLSession
-#from bs4 import BeautifulSoup
-from selenium import webdriver as wd
-import chromedriver_binary
+import pandas as pd
+import re
+from selenium import webdriver
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 
-year = input("enter year: ")
-year = str(year)
-
-card_set = input("card_set: ")
-card_set = str(card_set)
-
-character = input("Choose Character: ")
-character = str(character)
-my_search = year+" "+card_set+" "+character
+# Ask the user for input
+year = input("Enter year: ")
+card_set = input("Enter card set: ")
+character = input("Choose character: ")
+my_search = f"{year} {card_set} {character}"
 print(my_search)
 
+# Initialize the web driver
+wd = webdriver.Chrome()
 
-wd = wd.Chrome()
-
+# Set an implicit wait time for the web driver
 wd.implicitly_wait(10)
 
-wd.get("http://www.amazon.com")
+# Navigate to Amazon.com
+wd.get("https://www.amazon.com/")
 
-#locality for order
-wd.find_element(By.ID,"nav-global-location-popover-link").click()
-postcode_field = wd.find_element(By.ID,"GLUXZipUpdateInput")
+# Set the location to search results
+wd.find_element(By.ID, "nav-global-location-popover-link").click()
+postcode_field = wd.find_element(By.ID, "GLUXZipUpdateInput")
 postcode_field.send_keys("91423")
-wd.find_element(By.ID,"GLUXZipUpdate").click() 
+wd.find_element(By.ID, "GLUXZipUpdate").click()
 close_element = wd.find_element(By.CSS_SELECTOR, "button.a-button-text")
 close_element.click()
 
-wd.implicitly_wait(10)
-
-search_field = wd.find_element(By.NAME, "field-keywords").send_keys(my_search)
+# Search for the input
+search_field = wd.find_element(By.NAME, "field-keywords")
+search_field.send_keys(my_search)
 search_button_click = wd.find_element(By.ID, "nav-search-submit-button")
 search_button_click.click()
 
-wd.implicitly_wait(10)
+# Extract the search results with BeautifulSoup
+soup = BeautifulSoup(wd.page_source, "html.parser")
+web_page = soup.findAll("div", {"data-component-type": "s-search-result"})
 
-#wd.get("https://www.amazon.com/s?k={year}+{card_set}+{character}&crid=2XB82YGA8S9LV&sprefix={year}+{card_set}+{character}%2Caps%2C163&ref=nb_sb_noss")
+# Create a list to store the search results
+results_list = []
 
-#beatiful soup
-from bs4 import BeautifulSoup
-import re
-#regular expression
-
-soup = BeautifulSoup(wd.page_source, 'html.parser')
-
-web_page = soup.findAll("div",{"data-component-type": "s-search-result"})
-#print(results)
+# Loop through the search results and extract the relevant data
 for result in web_page:
     title = result.find("span", {"class": "a-size-base-plus a-color-base a-text-normal"})
-    wolverine = result.find("span", text = re.compile(character))
-    print(title.text)
-    print(wolverine)
+    price = result.find("span", {"class": "a-offscreen"})
+    url = result.find("a", {"class": "a-link-normal s-no-outline"})
+    character_match = result.find("span", text=re.compile(character, re.IGNORECASE))
+    if title and price and url and character_match:
+        title_text = title.text.strip()
+        price_text = price.text.strip()
+        url_text = "https://www.amazon.com" + url["href"]
+        character_text = character_match.text.strip()
+        print(f"{title_text} - {price_text} - {url_text} - {character_text}")
+        results_list.append({"Title": title_text, "Price": price_text, "URL": url_text, "Character": character_text})
 
+# Close the web driver
+wd.quit()
+
+# Convert the search results to a pandas DataFrame
+df = pd.DataFrame(results_list)
+
+# Drop rows without a price
+df = df.dropna(subset=["Price"])
+
+# Save the search results to a CSV file
+df.to_csv("marvel_cards.csv", index=False)
 
 
 
